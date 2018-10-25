7a03febdcaaba3617eb854e987334394f8f99e9a
sousagomide <sousagomide@gmail.com>
2018-10-25 16:50:44 -0300

    Initial commit

---
+++ src/main/java/br/edu/ifgoiano/substituicao/SubstituicaoRestApplication.java
@@ -0,0 +1,12 @@
+package br.edu.ifgoiano.substituicao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubstituicaoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubstituicaoRestApplication.class, args);
+	}
+}
