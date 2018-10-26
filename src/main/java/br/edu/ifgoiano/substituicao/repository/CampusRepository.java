7c448dea8d9b9878bbbe4423aa07929972037a63
Rodrigo Gomide <sousagomide@gmail.com>
2018-10-26 12:17:59 -0300

    Update src/main/java/br/edu/ifgoiano/substituicao/repository/CampusRepository.java

---
+++ src/main/java/br/edu/ifgoiano/substituicao/repository/CampusRepository.java
@@ -0,0 +1,10 @@
+package br.edu.ifgoiano.substituicao.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CampusRepository extends CrudRepository<CampusRepository, Long>{
+
+	
+	
+	
+}
