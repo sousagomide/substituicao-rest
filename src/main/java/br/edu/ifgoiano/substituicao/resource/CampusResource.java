418b2990f2081084a9b7a9d898411d255b999d03
Rodrigo Gomide <sousagomide@gmail.com>
2018-10-26 13:50:36 -0300

    Update src/main/java/br/edu/ifgoiano/substituicao/resource/CampusResource.java

--- src/main/java/br/edu/ifgoiano/substituicao/resource/CampusResource.java
+++ src/main/java/br/edu/ifgoiano/substituicao/resource/CampusResource.java
@@ -1,11 +1,19 @@
 package br.edu.ifgoiano.substituicao.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.edu.ifgoiano.substituicao.model.Campus;
 import br.edu.ifgoiano.substituicao.repository.CampusRepository;
@@ -20,6 +28,21 @@ public class CampusResource {
 	@GetMapping
 	public List<Campus> listar(){
 		return repository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Campus> registrar(@RequestBody Campus campus, HttpServletResponse response){
+		Campus registro = repository.save(campus);
+		//Cria e adiciona um Location no HttpServletResponse. Usado quando um novo recurso é criado
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(registro.getId()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		//Retorna o Campus registrado
+		return ResponseEntity.created(uri).body(registro);
+	}
+	
+	@GetMapping("/{id}")
+	public Campus listarId(@PathVariable Long id) {
+		return repository.findById(id).get();
 	}
 	
 	
