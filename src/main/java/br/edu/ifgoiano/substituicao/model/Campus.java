153d0f260f9b8736a792880f01c9921ce8dc6c72
Rodrigo Gomide <sousagomide@gmail.com>
2018-10-26 11:29:06 -0300

    Update src/main/java/br/edu/ifgoiano/substituicao/model/Campus.java

---
+++ src/main/java/br/edu/ifgoiano/substituicao/model/Campus.java
@@ -0,0 +1,14 @@
+package br.edu.ifgoiano.substituicao.model;
+
+import javax.persistence.Entity;
+
+@Entity(name="campus")
+public class Campus {
+
+	private Long id;
+	private String instituicao;
+	private String nome;
+	
+	
+	
+}
