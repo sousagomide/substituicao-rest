a7bf69b1e1a08f05c60283547efe0d7b4d18716f
Rodrigo Gomide <sousagomide@gmail.com>
2018-10-26 11:29:10 -0300

    Update src/main/java/br/edu/ifgoiano/substituicao/SubstituicaoRestApplication.java

--- src/main/java/br/edu/ifgoiano/substituicao/SubstituicaoRestApplication.java
+++ src/main/java/br/edu/ifgoiano/substituicao/SubstituicaoRestApplication.java
@@ -1,12 +1,26 @@
 package br.edu.ifgoiano.substituicao;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class SubstituicaoRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SubstituicaoRestApplication.class, args);
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost/dbsubstituicao?useTimezone=true&serverTimezone=UTC&useSSL=false");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		return dataSource;
 	}
 }
