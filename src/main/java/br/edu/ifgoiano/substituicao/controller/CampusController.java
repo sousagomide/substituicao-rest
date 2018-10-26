9b825129bb7a4678958acd56a545e88ec28bd54a
Rodrigo Gomide <sousagomide@gmail.com>
2018-10-26 12:17:49 -0300

    Update src/main/java/br/edu/ifgoiano/substituicao/controller/CampusController.java

---
+++ src/main/java/br/edu/ifgoiano/substituicao/controller/CampusController.java
@@ -0,0 +1,12 @@
+package br.edu.ifgoiano.substituicao.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.edu.ifgoiano.substituicao.repository.CampusRepository;
+
+public class CampusController {
+
+	@Autowired
+	private CampusRepository repository;
+	
+}
