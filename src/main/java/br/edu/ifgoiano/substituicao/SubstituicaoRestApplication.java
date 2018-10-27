78ef14a0173b3de1ed40a1e81844cacb3beec854
Rodrigo Gomide <sousagomide@gmail.com>
2018-10-27 19:35:33 -0300

    Update src/main/java/br/edu/ifgoiano/substituicao/SubstituicaoRestApplication.java

--- src/main/java/br/edu/ifgoiano/substituicao/SubstituicaoRestApplication.java
+++ src/main/java/br/edu/ifgoiano/substituicao/SubstituicaoRestApplication.java
@@ -1,11 +1,7 @@
 package br.edu.ifgoiano.substituicao;
 
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class SubstituicaoRestApplication {
