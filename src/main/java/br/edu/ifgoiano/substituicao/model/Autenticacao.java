d41ba2ff2e8e4e4917533f8eb272b841a1874033
Rodrigo Gomide <sousagomide@gmail.com>
2018-10-27 10:52:03 -0300

    Update src/main/java/br/edu/ifgoiano/substituicao/model/Autenticacao.java

---
+++ src/main/java/br/edu/ifgoiano/substituicao/model/Autenticacao.java
@@ -0,0 +1,90 @@
+package br.edu.ifgoiano.substituicao.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "autenticacao")
+public class Autenticacao {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String usuario;
+	
+	private String senha;
+	
+	@ManyToOne(cascade = CascadeType.ALL) @JoinColumn(name = "idperfil")
+	private Perfil idPerfil;
+
+	public Autenticacao(Long id, String usuario, String senha, Perfil idPerfil) {
+		this.id = id;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.idPerfil = idPerfil;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Perfil getIdPerfil() {
+		return idPerfil;
+	}
+
+	public void setIdPerfil(Perfil idPerfil) {
+		this.idPerfil = idPerfil;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autenticacao other = (Autenticacao) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
