57a2c69b01c102b70bb92ac8d2da1e90e36eac68
Rodrigo Gomide <sousagomide@gmail.com>
2018-10-27 14:46:33 -0300

    Update src/main/java/br/edu/ifgoiano/substituicao/model/Turma.java

---
+++ src/main/java/br/edu/ifgoiano/substituicao/model/Turma.java
@@ -0,0 +1,80 @@
+package br.edu.ifgoiano.substituicao.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "turma")
+public class Turma {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private Integer ano;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "idcurso")
+	private Curso idCurso;
+
+	public Turma(Long id, Integer ano, Curso idCurso) {
+		this.id = id;
+		this.ano = ano;
+		this.idCurso = idCurso;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+	public Curso getIdCurso() {
+		return idCurso;
+	}
+
+	public void setIdCurso(Curso idCurso) {
+		this.idCurso = idCurso;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Turma other = (Turma) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
