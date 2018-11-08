9f16d09bb7d2d7a1cb7476c3ed58fa3cd57d30f9
Murilo Viana <40148042+murilocarrijosilva@users.noreply.github.com>
2018-11-08 17:02:57 -0200

    Update src/main/java/br/edu/ifgoiano/substituicao/repository/CursoRepository.java

---
+++ src/main/java/br/edu/ifgoiano/substituicao/repository/CursoRepository.java
@@ -0,0 +1,7 @@
+package br.edu.ifgoiano.substituicao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.edu.ifgoiano.substituicao.model.Curso;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {}
