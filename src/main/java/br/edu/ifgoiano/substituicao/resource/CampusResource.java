b3860a45cb914d8fa413c07194ef31a5365c07b7
sousagomide <sousagomide@gmail.com>
2018-11-08 17:41:10 -0200

    Update src/main/java/br/edu/ifgoiano/substituicao/resource/CampusResource.java

--- src/main/java/br/edu/ifgoiano/substituicao/resource/CampusResource.java
+++ src/main/java/br/edu/ifgoiano/substituicao/resource/CampusResource.java
@@ -1,6 +1,5 @@
 package br.edu.ifgoiano.substituicao.resource;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.edu.ifgoiano.substituicao.event.RecursoCriadoEvent;
 import br.edu.ifgoiano.substituicao.model.Campus;
