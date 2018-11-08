463ee74273a46ae9b0b54f7241b0307af926e54e
sousagomide <sousagomide@gmail.com>
2018-11-08 18:14:01 -0200

    Update src/main/java/br/edu/ifgoiano/substituicao/resource/CampusResource.java

--- src/main/java/br/edu/ifgoiano/substituicao/resource/CampusResource.java
+++ src/main/java/br/edu/ifgoiano/substituicao/resource/CampusResource.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifgoiano.substituicao.event.RecursoCriadoEvent;
@@ -46,6 +48,12 @@ public class CampusResource {
 	@GetMapping("/{id}")
 	public Campus listarId(@PathVariable Long id) {
 		return repository.findById(id).get();
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long id) {
+		repository.deleteById(id);
 	}
 	
 	
