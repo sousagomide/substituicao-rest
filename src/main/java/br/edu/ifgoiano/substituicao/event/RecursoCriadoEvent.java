fef835ff442de0e92ad9b52e4f654f5bc4fa9bb5
sousagomide <sousagomide@gmail.com>
2018-11-08 16:56:58 -0200

    Update src/main/java/br/edu/ifgoiano/substituicao/event/RecursoCriadoEvent.java

---
+++ src/main/java/br/edu/ifgoiano/substituicao/event/RecursoCriadoEvent.java
@@ -0,0 +1,27 @@
+package br.edu.ifgoiano.substituicao.event;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationEvent;
+
+public class RecursoCriadoEvent extends ApplicationEvent {
+
+	private static final long serialVersionUID = 1L;
+
+	private HttpServletResponse response;
+	private Long id;
+
+	public RecursoCriadoEvent(Object source, HttpServletResponse response, Long id) {
+		super(source);
+		this.id = id;
+		this.response = response;
+	}
+
+	public HttpServletResponse getResponse() {
+		return response;
+	}
+
+	public Long getId() {
+		return id;
+	}
+}
