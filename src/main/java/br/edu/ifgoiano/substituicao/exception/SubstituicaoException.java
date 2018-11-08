7691dc4c575f3053be5573a1d3070b454ea3b461
Murilo Viana <40148042+murilocarrijosilva@users.noreply.github.com>
2018-11-08 17:02:42 -0200

    Update src/main/java/br/edu/ifgoiano/substituicao/exception/SubstituicaoException.java

--- src/main/java/br/edu/ifgoiano/substituicao/exception/SubstituicaoException.java
+++ src/main/java/br/edu/ifgoiano/substituicao/exception/SubstituicaoException.java
@@ -1,13 +1,18 @@
 package br.edu.ifgoiano.substituicao.exception;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -24,6 +29,16 @@ public class SubstituicaoException extends ResponseEntityExceptionHandler{
 		String mensagemDesenvolvedor = ex.getCause().toString();
 		
 		return handleExceptionInternal(ex, new Error(mensagem, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	// Trata a exception encontrada ao tentar remover um recurso inexistente.
+	@ExceptionHandler({ EmptyResultDataAccessException.class })
+	public ResponseEntity<Object> handleEmptyResultDataAcessException(EmptyResultDataAccessException ex, WebRequest request) {
+		
+		String mensagem = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.toString();
+		
+		return handleExceptionInternal(ex, new Error(mensagem, mensagemDesenvolvedor), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 	
 }
