8760fef01766025987ddd134f9f3c346c45e0e3c
Rodrigo Gomide <sousagomide@gmail.com>
2018-11-09 18:27:01 -0200

    Update src/main/java/br/edu/ifgoiano/substituicao/repository/impl/CampusRepositoryImpl.java

--- src/main/java/br/edu/ifgoiano/substituicao/repository/impl/CampusRepositoryImpl.java
+++ src/main/java/br/edu/ifgoiano/substituicao/repository/impl/CampusRepositoryImpl.java
@@ -11,6 +11,9 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.util.StringUtils;
 
 import br.edu.ifgoiano.substituicao.model.Campus;
@@ -23,14 +26,33 @@ public class CampusRepositoryImpl implements CampusRepositoryQuery{
 	private EntityManager manager;
 
 	@Override
-	public List<Campus> listar(CampusFilter campusFilter) {
+	public Page<Campus> listar(CampusFilter campusFilter, Pageable pageable) {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		CriteriaQuery<Campus> criteria = builder.createQuery(Campus.class);
 		Root<Campus> root = criteria.from(Campus.class);
 		Predicate[] predicates = criarRestricoes(campusFilter, builder, root);
 		criteria.where(predicates);
 		TypedQuery<Campus> query = manager.createQuery(criteria);
-		return query.getResultList();
+		adicionarPaginacao(query, pageable);
+		return new PageImpl<>(query.getResultList(), pageable, total(campusFilter));
+	}
+
+	private Long total(CampusFilter campusFilter) {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+		Root<Campus> root = criteria.from(Campus.class);
+		Predicate[] predicates = criarRestricoes(campusFilter, builder, root);
+		criteria.where(predicates);
+		criteria.select(builder.count(root));
+		return manager.createQuery(criteria).getSingleResult();
+	}
+
+	private void adicionarPaginacao(TypedQuery<Campus> query, Pageable pageable) {
+		int paginaAtual = pageable.getPageNumber();
+		int totalRegistrosPorPagina = pageable.getPageSize();
+		int primeiroRegistro = paginaAtual * totalRegistrosPorPagina;
+		query.setFirstResult(primeiroRegistro);
+		query.setMaxResults(totalRegistrosPorPagina);
 	}
 
 	private Predicate[] criarRestricoes(CampusFilter campusFilter, CriteriaBuilder builder, Root<Campus> root) {
