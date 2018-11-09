b2d8f388a3a96ba5dd6c7a68d51cc0aeef733453
Rodrigo Gomide <sousagomide@gmail.com>
2018-11-09 18:08:46 -0200

    Update src/main/java/br/edu/ifgoiano/substituicao/repository/CampusRepository.java

--- src/main/java/br/edu/ifgoiano/substituicao/repository/CampusRepository.java
+++ src/main/java/br/edu/ifgoiano/substituicao/repository/CampusRepository.java
@@ -3,5 +3,6 @@ package br.edu.ifgoiano.substituicao.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.ifgoiano.substituicao.model.Campus;
+import br.edu.ifgoiano.substituicao.repository.query.CampusRepositoryQuery;
 
-public interface CampusRepository extends JpaRepository<Campus, Long>{}
+public interface CampusRepository extends JpaRepository<Campus, Long>, CampusRepositoryQuery{}
