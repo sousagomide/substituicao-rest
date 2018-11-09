25a7a89d1480dfc9077c9a83423bae0bbd366130
Rodrigo Gomide <sousagomide@gmail.com>
2018-11-09 18:09:02 -0200

    Update src/main/java/br/edu/ifgoiano/substituicao/repository/impl/CampusRepositoryImpl.java

---
+++ src/main/java/br/edu/ifgoiano/substituicao/repository/impl/CampusRepositoryImpl.java
@@ -0,0 +1,45 @@
+package br.edu.ifgoiano.substituicao.repository.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.util.StringUtils;
+
+import br.edu.ifgoiano.substituicao.model.Campus;
+import br.edu.ifgoiano.substituicao.repository.filter.CampusFilter;
+import br.edu.ifgoiano.substituicao.repository.query.CampusRepositoryQuery;
+
+public class CampusRepositoryImpl implements CampusRepositoryQuery{
+	
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	public List<Campus> listar(CampusFilter campusFilter) {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<Campus> criteria = builder.createQuery(Campus.class);
+		Root<Campus> root = criteria.from(Campus.class);
+		Predicate[] predicates = criarRestricoes(campusFilter, builder, root);
+		criteria.where(predicates);
+		TypedQuery<Campus> query = manager.createQuery(criteria);
+		return query.getResultList();
+	}
+
+	private Predicate[] criarRestricoes(CampusFilter campusFilter, CriteriaBuilder builder, Root<Campus> root) {
+		List<Predicate> predicates = new ArrayList<>();
+		if(!StringUtils.isEmpty(campusFilter.getInstituicao()))
+			predicates.add(builder.like(builder.lower(root.get("instituicao")), campusFilter.getInstituicao() + "%"));
+		if(!StringUtils.isEmpty(campusFilter.getNome()))
+			predicates.add(builder.like(builder.lower(root.get("nome")), campusFilter.getNome() + "%"));
+		return predicates.toArray(new Predicate[predicates.size()]);
+	}
+
+}
