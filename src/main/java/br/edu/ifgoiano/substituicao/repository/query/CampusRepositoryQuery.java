9298350a1b14e1bbdce9e3e5ab8e13b1c6636a05
Rodrigo Gomide <sousagomide@gmail.com>
2018-11-09 18:27:06 -0200

    Update src/main/java/br/edu/ifgoiano/substituicao/repository/query/CampusRepositoryQuery.java

--- src/main/java/br/edu/ifgoiano/substituicao/repository/query/CampusRepositoryQuery.java
+++ src/main/java/br/edu/ifgoiano/substituicao/repository/query/CampusRepositoryQuery.java
@@ -1,12 +1,13 @@
 package br.edu.ifgoiano.substituicao.repository.query;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import br.edu.ifgoiano.substituicao.model.Campus;
 import br.edu.ifgoiano.substituicao.repository.filter.CampusFilter;
 
 public interface CampusRepositoryQuery {
 	
-	public List<Campus> listar(CampusFilter campusFilter);
+	public Page<Campus> listar(CampusFilter campusFilter, Pageable pageable);
 
 }
