66e3232537d1cee493bd650ce499ce76f9521052
Rodrigo Gomide <sousagomide@gmail.com>
2018-11-09 18:08:52 -0200

    Update src/main/java/br/edu/ifgoiano/substituicao/repository/filter/CampusFilter.java

---
+++ src/main/java/br/edu/ifgoiano/substituicao/repository/filter/CampusFilter.java
@@ -0,0 +1,24 @@
+package br.edu.ifgoiano.substituicao.repository.filter;
+
+public class CampusFilter {
+
+	private String nome;
+	private String instituicao;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getInstituicao() {
+		return instituicao;
+	}
+
+	public void setInstituicao(String instituicao) {
+		this.instituicao = instituicao;
+	}
+
+}
