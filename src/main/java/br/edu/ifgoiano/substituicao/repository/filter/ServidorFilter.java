650d8e4119187bbdb04938826796751a0b096b78
Murilo Viana <40148042+murilocarrijosilva@users.noreply.github.com>
2018-11-17 19:35:01 -0200

    Update src/main/java/br/edu/ifgoiano/substituicao/repository/filter/ServidorFilter.java

---
+++ src/main/java/br/edu/ifgoiano/substituicao/repository/filter/ServidorFilter.java
@@ -0,0 +1,63 @@
+package br.edu.ifgoiano.substituicao.repository.filter;
+
+import br.edu.ifgoiano.substituicao.model.Autenticacao;
+import br.edu.ifgoiano.substituicao.model.enumeration.CategoriaServidor;
+
+public class ServidorFilter {
+
+	private String nome;
+	private String siape;
+	private CategoriaServidor categoria;
+	private String funcao;
+	private String email;
+	private Autenticacao idAutenticacao;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSiape() {
+		return siape;
+	}
+
+	public void setSiape(String siape) {
+		this.siape = siape;
+	}
+
+	public CategoriaServidor getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(CategoriaServidor categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getFuncao() {
+		return funcao;
+	}
+
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Autenticacao getIdAutenticacao() {
+		return idAutenticacao;
+	}
+
+	public void setIdAutenticacao(Autenticacao idAutenticacao) {
+		this.idAutenticacao = idAutenticacao;
+	}
+
+}
