33a623ef126599bbd0b5acf72b43bf7951e6505e
Murilo Viana <40148042+murilocarrijosilva@users.noreply.github.com>
2018-11-17 19:34:48 -0200

    Update src/main/java/br/edu/ifgoiano/substituicao/repository/AutenticacaoRepository.java

--- src/main/java/br/edu/ifgoiano/substituicao/repository/AutenticacaoRepository.java
+++ src/main/java/br/edu/ifgoiano/substituicao/repository/AutenticacaoRepository.java
@@ -3,5 +3,6 @@ package br.edu.ifgoiano.substituicao.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.ifgoiano.substituicao.model.Autenticacao;
+import br.edu.ifgoiano.substituicao.repository.query.AutenticacaoRepositoryQuery;
 
-public interface AutenticacaoRepository extends JpaRepository<Autenticacao, Long> {}
+public interface AutenticacaoRepository extends JpaRepository<Autenticacao, Long>, AutenticacaoRepositoryQuery{}
