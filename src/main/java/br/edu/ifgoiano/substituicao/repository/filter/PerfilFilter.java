fe24d4bb3b844031d8a46f0de413a8f40fd6800c
Murilo Viana <40148042+murilocarrijosilva@users.noreply.github.com>
2018-11-17 19:35:00 -0200

    Update src/main/java/br/edu/ifgoiano/substituicao/repository/filter/PerfilFilter.java

---
+++ src/main/java/br/edu/ifgoiano/substituicao/repository/filter/PerfilFilter.java
@@ -0,0 +1,15 @@
+package br.edu.ifgoiano.substituicao.repository.filter;
+
+public class PerfilFilter {
+
+	private String descricao;
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
