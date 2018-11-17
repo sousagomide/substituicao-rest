6b31e66d354a452b69e05b28790ff3796c0f407d
Murilo Viana <40148042+murilocarrijosilva@users.noreply.github.com>
2018-11-17 19:35:28 -0200

    Update src/main/java/br/edu/ifgoiano/substituicao/resource/CursoResource.java

--- src/main/java/br/edu/ifgoiano/substituicao/resource/CursoResource.java
+++ src/main/java/br/edu/ifgoiano/substituicao/resource/CursoResource.java
@@ -1,17 +1,20 @@
 package br.edu.ifgoiano.substituicao.resource;
 
 import java.net.URI;
-import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -20,6 +23,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.edu.ifgoiano.substituicao.model.Curso;
 import br.edu.ifgoiano.substituicao.repository.CursoRepository;
+import br.edu.ifgoiano.substituicao.repository.filter.CursoFilter;
 
 @RestController
 @RequestMapping("/cursos")
@@ -29,8 +33,8 @@ public class CursoResource {
 	private CursoRepository repository;
 	
 	@GetMapping
-	public List<Curso> listar() {
-		return repository.findAll();
+	public Page<Curso> listar(CursoFilter cursoFilter, Pageable pageable) {
+		return repository.listar(cursoFilter, pageable);
 	}
 	
 	@PostMapping
@@ -54,5 +58,14 @@ public class CursoResource {
 		repository.deleteById(id);
 	}
 	
+	@PutMapping("/{id}")
+	public ResponseEntity<Curso> atualizar(@RequestBody Curso curso, @PathVariable Long id) {
+		Optional<Curso> cursoOptional = repository.findById(id);
+		if (!cursoOptional.isPresent())
+			return ResponseEntity.notFound().build();
+		curso.setId(id);
+		repository.save(curso);
+		return ResponseEntity.noContent().build();
+	}
 	
 }
