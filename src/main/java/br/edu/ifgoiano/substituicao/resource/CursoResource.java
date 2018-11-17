3020754e94b5738ab1acf797ec90c04993cb39f5
Murilo Viana <40148042+murilocarrijosilva@users.noreply.github.com>
2018-11-17 20:01:51 -0200

    Update CursoResource.java

--- src/main/java/br/edu/ifgoiano/substituicao/resource/CursoResource.java
+++ src/main/java/br/edu/ifgoiano/substituicao/resource/CursoResource.java
@@ -1,11 +1,11 @@
 package br.edu.ifgoiano.substituicao.resource;
 
-import java.net.URI;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -19,8 +19,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.edu.ifgoiano.substituicao.event.RecursoCriadoEvent;
 import br.edu.ifgoiano.substituicao.model.Curso;
 import br.edu.ifgoiano.substituicao.repository.CursoRepository;
 import br.edu.ifgoiano.substituicao.repository.filter.CursoFilter;
@@ -32,6 +32,9 @@ public class CursoResource {
 	@Autowired
 	private CursoRepository repository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public Page<Curso> listar(CursoFilter cursoFilter, Pageable pageable) {
 		return repository.listar(cursoFilter, pageable);
@@ -40,16 +43,16 @@ public class CursoResource {
 	@PostMapping
 	public ResponseEntity<Curso> registrar(@RequestBody Curso curso, HttpServletResponse response) {
 		Curso registro = repository.save(curso);
-		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(registro.getId()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
-		
-		return ResponseEntity.created(uri).body(registro);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, registro.getId()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(registro);
 	}
 	
 	@GetMapping("/{id}")
-	public Curso listarId(@PathVariable Long id) {
-		return repository.findById(id).get();
+	public ResponseEntity<Curso> listarId(@PathVariable Long id) {
+		Optional<Curso> response = repository.findById(id);
+		if (!response.isPresent())
+			return ResponseEntity.notFound().build();
+		return ResponseEntity.ok().body(response.get());
 	}
 	
 	@DeleteMapping("/{id}")
