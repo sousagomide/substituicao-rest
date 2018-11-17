c70567e5fd25d773ac491707af6ef90cceb8e731
Murilo Viana <40148042+murilocarrijosilva@users.noreply.github.com>
2018-11-17 19:34:54 -0200

    Update src/main/java/br/edu/ifgoiano/substituicao/repository/filter/AutenticacaoFilter.java

---
+++ src/main/java/br/edu/ifgoiano/substituicao/repository/filter/AutenticacaoFilter.java
@@ -0,0 +1,35 @@
+package br.edu.ifgoiano.substituicao.repository.filter;
+
+import br.edu.ifgoiano.substituicao.model.Perfil;
+
+public class AutenticacaoFilter {
+
+	private String usuario;
+	private String senha;
+	private Perfil idPerfil;
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Perfil getIdPerfil() {
+		return idPerfil;
+	}
+
+	public void setIdPerfil(Perfil idPerfil) {
+		this.idPerfil = idPerfil;
+	}
+
+}
